b92e0b5d238c7f025af5fdf2c79efba22a299fae
gmokhtar <10-gmokhtar@users.noreply.gitlab.mines-albi.fr>
2020-10-16 11:28:35 +0200

    ajout de la page "créer un patient"

--- PTUT-AGGIR/src/app/app.module.ts
+++ PTUT-AGGIR/src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './components/app.component';
 import { ListeResidentComponent } from './components/liste-resident/liste-resident.component';
 import { NewPatientComponent } from './components/new-patient/new-patient.component';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from "@angular/forms";
+import { HistoriquePatientComponent } from './components/historique-patient/historique-patient.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListeResidentComponent,
-    NewPatientComponent
+    NewPatientComponent,
+    HistoriquePatientComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ PTUT-AGGIR/src/app/components/historique-patient/historique-patient.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-historique-patient',
+  templateUrl: './historique-patient.component.html',
+  styleUrls: ['./historique-patient.component.scss']
+})
+export class HistoriquePatientComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
